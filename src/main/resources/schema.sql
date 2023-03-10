2bf6c7f5e7dae59f1a4a30135fb761f2db4ab00b
Denis Khremin <denis.khremin@heyfavor.com>
2023-03-10 10:17:49 -0800

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `first_table`
+(
+    `id`      bigint(11) NOT NULL AUTO_INCREMENT,
+    `message` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
